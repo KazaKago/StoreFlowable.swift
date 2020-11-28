1cd3c986d69f330d13304cc140cec7703779ba63
Kensuke Tamura <kazakago@gmail.com>
2020-11-28 21:57:07 +0900

    add sample project.

---
+++ Sample/Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Kensuke Tamura on 2020/11/28.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
