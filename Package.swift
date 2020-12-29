cb3d40011d9438c8064974af5eff76fc4905273f
Kensuke Tamura <kazakago@gmail.com>
2020-12-29 13:28:08 +0900

    support Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "StoreFlowable",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+        .tvOS(.v14),
+        .watchOS(.v7),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +20,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/rizumita/CombineAsync.git", from: "0.1.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StoreFlowable",
-            dependencies: []),
+            dependencies: ["CombineAsync"]),
         .testTarget(
             name: "StoreFlowableTests",
             dependencies: ["StoreFlowable"]),
