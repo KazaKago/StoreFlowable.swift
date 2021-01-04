c6b4a3a3531eb831746dc39084c359a466217ed5
Kensuke Tamura <kazakago@gmail.com>
2021-01-04 18:25:51 +0900

    fix for SwiftPM

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/rizumita/CombineAsync.git", from: "0.1.5"),
+        .package(url: "https://github.com/groue/CombineExpectations.git", from: "0.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,6 +31,6 @@ let package = Package(
             dependencies: ["CombineAsync"]),
         .testTarget(
             name: "StoreFlowableTests",
-            dependencies: ["StoreFlowable"]),
+            dependencies: ["StoreFlowable", "CombineExpectations"]),
     ]
 )
